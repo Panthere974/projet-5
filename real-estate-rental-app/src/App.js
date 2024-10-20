0502b2e87278334677d8d37e17ce676f5a464e31
Cédric Payet <cedricpayet.dev@gmail.com>
2024-10-20 10:45:01 +0200

    NavBar : done

--- real-estate-rental-app/src/App.js
+++ real-estate-rental-app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.scss';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import styles from './App.module.scss';
+import Navbar from './components/Navbar/Navbar.js';
 import Home from './pages/Home.js';
 import Accommodation from './pages/Accommodation.js';
 import About from './pages/About.js';
@@ -10,12 +11,15 @@ import Error404 from './pages/Error404.js';
 function App() {
   return (
     <Router>
+      <Navbar/>
+      <div className={styles.main}>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/accommodation" element={<Accommodation />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error404 />} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/accommodation" element={<Accommodation/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="*" element={<Error404/>}/>
       </Routes>
+      </div>
     </Router>
   );
 }
