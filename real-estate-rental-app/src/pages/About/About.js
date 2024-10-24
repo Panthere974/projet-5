e1abe1f4e20ce7200be19112bf9b9aadfc766d41
Cédric Payet <cedricpayet.dev@gmail.com>
2024-10-24 21:42:31 +0200

    responsive about : done

--- real-estate-rental-app/src/pages/About/About.js
+++ real-estate-rental-app/src/pages/About/About.js
@@ -2,24 +2,25 @@ import React from 'react';
 import styles from './About.module.scss';
 import Banner from '../../components/Banner/Banner.js';
 import Collapse from '../../components/Collapse/Collapse.js';
-import backgroundImage2 from '../../assets/background_image_2.svg';
 
 const About = () => {
   return (
     <div className={styles.about}>
-      <Banner title='' backgroundImage={backgroundImage2}></Banner>
-      <Collapse title='Fiabilité' 
-      children='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.'>
-      </Collapse>
-      <Collapse title='Respect'
-      children='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'>
-      </Collapse>
-      <Collapse title='Service'
-      children='La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.'>
-      </Collapse>
-      <Collapse title='Sécurité'
-      children="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.">
-      </Collapse>
+      <Banner type='about'></Banner>
+      <div className={styles.collapses}>
+        <Collapse title='Fiabilité' 
+        children='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.'>
+        </Collapse>
+        <Collapse title='Respect'
+        children='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'>
+        </Collapse>
+        <Collapse title='Service'
+        children='La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.'>
+        </Collapse>
+        <Collapse title='Sécurité'
+        children="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.">
+        </Collapse>
+      </div>
     </div>
   );
 };
